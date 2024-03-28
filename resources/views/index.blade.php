ae2986fbe9b4e8897b70be6ed04ae12417c1422d
Work <work@Chriss-MacBook-Pro.local>
2024-03-28 13:52:10 -0700

    initial commit for puzzlesite

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@include('Layouts.header')
+
+
+<div>
+	<div class="typewriter font-mono text-lg text-gray-800">
+	    <span id="text"></span>
+	    <span class="cursor">|</span>
+	</div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+
+
+
+Route::get('/', [IndexController::class, 'index']);
+
+
 
 require __DIR__.'/auth.php';
